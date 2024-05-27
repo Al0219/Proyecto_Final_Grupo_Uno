17e0aa5b5c65747491595d2448b72960b04cb83b
Al0219 <161105964+Al0219@users.noreply.github.com>
2024-05-26 21:21:05 -0600

    Agregar Menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner leer = new Scanner(System.in);
+        int opcion = 0;
+        do {
+            System.out.println("Ingrese una opcion ");
+            System.out.println("Opcion 1 - Registar un nuevo cliente ");
+            System.out.println("Opcion 2 - Consultar los clientes ");
+            System.out.println("Opcion 3 - Salir del programa");
+            opcion = leer.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        }while(opcion != 3);
     }
 }
