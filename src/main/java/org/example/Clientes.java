b5213642791d804ca2f576eca959e8f5663bdd6a
Al0219 <161105964+Al0219@users.noreply.github.com>
2024-05-27 21:05:07 -0600

    PruebaError

--- src/main/java/org/example/Clientes.java
+++ src/main/java/org/example/Clientes.java
@@ -1,5 +1,67 @@
 package org.example;
 
 public class Clientes {
+    private String nombreCompletoCliente = null;
+    private String numeroDPI = null;
+    private String fechaDePago = null;
+    private String fechaDeFinalizacion = null;
+    private String formaDePago = null;
+    private String tiempoPagado = null;
+    private Boolean activo = false;
 
+    public String getNombreCompletoCliente() {
+        return nombreCompletoCliente;
+    }
+
+    public void setNombreCompletoCliente(String nombreCompletoCliente) {
+        this.nombreCompletoCliente = nombreCompletoCliente;
+    }
+
+    public String getNumeroDPI() {
+        return numeroDPI;
+    }
+
+    public void setNumeroDPI(String numeroDPI) {
+        this.numeroDPI = numeroDPI;
+    }
+
+    public String getFechaDePago() {
+        return fechaDePago;
+    }
+
+    public void setFechaDePago(String fechaDePago) {
+        this.fechaDePago = fechaDePago;
+    }
+
+    public String getFechaDeFinalizacion() {
+        return fechaDeFinalizacion;
+    }
+
+    public void setFechaDeFinalizacion(String fechaDeFinalizacion) {
+        this.fechaDeFinalizacion = fechaDeFinalizacion;
+    }
+
+    public String getFormaDePago() {
+        return formaDePago;
+    }
+
+    public void setFormaDePago(String formaDePago) {
+        this.formaDePago = formaDePago;
+    }
+
+    public String getTiempoPagado() {
+        return tiempoPagado;
+    }
+
+    public void setTiempoPagado(String tiempoPagado) {
+        this.tiempoPagado = tiempoPagado;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
