66fe178e6c23d182dd91f3af6d961b6cb649f1f4
David Tema <josedavidtema0@gmail.com>
2024-06-01 09:12:26 -0600

    arreglos en la clase clientes

--- src/main/java/principal/Clientes.java
+++ src/main/java/principal/Clientes.java
@@ -8,6 +8,7 @@ public class Clientes {
     private String numeroDPI = null;
     private Date fechaNacimiento = null;
 
+
     public String getNumeroDPI() {
         return numeroDPI;
     }
@@ -24,9 +25,7 @@ public class Clientes {
         this.fechaNacimiento = fechaNacimiento;
     }
 
-    public String getNombreCompletoCliente() {
-        return nombreCliente;
-    }
+
 
     public void setNombreCliente(String nombresCliente) {
         this.nombreCliente = nombresCliente;
@@ -40,50 +39,7 @@ public class Clientes {
         this.apellidoCliente = apellidosCliente;
     }
 
-    public void setNombreCompletoCliente(String nombre) {
-    }
 
-    public void setFechaDePago(String fechaDePago) {
-    }
-
-    public void setFechaDeFinalizacion(String fechaDeFinalizacion) {
-    }
-
-    public void setFormaDePago(String formaDePago) {
-    }
-
-    public void setTiempoPagado(String tiempoPagado) {
-    }
-
-    public void setActivo(boolean activo) {
-    }
-
-
-    public String getFechaDePago() {
-        String FechaDePago = null;
-        return FechaDePago;
-    }
-
-    public String getFechaDeFinalizacion() {
-        String FechaDeFinalizacion = null;
-        return FechaDeFinalizacion;
-    }
-
-    public String getFormaDePago() {
-        String FormaDePago = null;
-        return FormaDePago;
-
-    }
-
-    public String getTiempoPagado() {
-        String TiempoPagado = null;
-        return TiempoPagado;
-    }
-
-    public boolean getActivo() {
-        boolean Activo = Boolean.parseBoolean(null);
-        return Activo;
-    }
 
     public String getNombreCliente() {
         String NombreCliente = null;
