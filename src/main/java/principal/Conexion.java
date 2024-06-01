efdcc1734c6ada68f0578b9a1f62d77a65616d94
alexanderCanon <alexandercanon64@gmail.com>
2024-05-31 22:12:26 -0600

    Mejoras

--- src/main/java/principal/Conexion.java
+++ src/main/java/principal/Conexion.java
@@ -5,11 +5,10 @@ import java.sql.*;
 public class Conexion {
     //public static String driver = "com.mysql.jdbc.Driver";
     public String database = "gimnasio";
-    public String hostname = "mysql.webcindario.com";
-    //public String hostname = "localhost";
+    public String hostname = "localhost";
     public String port ="3306";
-    public String username = "gimnasio";
-    public String password ="equipoUno2024";
+    public String username = "root";
+    public String password ="";
     public String url = "jdbc:mysql://"+hostname+":"+port+"/"+database;
     public ConnectionImpl conex;
 
